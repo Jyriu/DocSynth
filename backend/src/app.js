089ec55266e5abe59be94cdd2e7b8a4514099776
PhilippeMaillot <maillotphilippe78@gmail.com>
2025-06-30 16:31:40 +0200

    feat(auth): add signup & login routes with JWT and bcrypt

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const authRoutes = require('./routes/auth.routes');
 
 app.use(express.json());
 
 // routes ici
+app.use('/auth', authRoutes);
 
 module.exports = app;
